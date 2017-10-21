bc069c84973ff796e2452c4094b003b99ffc6aad
Ryan Hayden <ryanhayden@ryans-mbp.attlocal.net>
2017-10-20 22:37:50 -0500

    updated html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,35 +13,34 @@ var config = {
 
 $("#addTrainBtn").on("click", function(){
 
-	
+
 	var trainName = $("#trainNameInput").val().trim();
 	var destination = $("#destinationInput").val().trim();
 	var firstTrainUnix = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
 	var frequency = $("#frequencyInput").val().trim();
 
-	
+
 	var newTrain = {
 		name:  trainName,
 		destination: destination,
 		firstTrain: firstTrainUnix,
-		frequency: frequency
-	}
+		frequency: frequency,
+	};
 
-	
 	trainData.push(newTrain);
 
 	console.log(newTrain.name);
-	console.log(newTrain.destination); 
+	console.log(newTrain.destination);
 	console.log(firstTrainUnix);
-	console.log(newTrain.frequency)
+	console.log(newTrain.frequency);
 
-	
+
 	$("#trainNameInput").val("");
 	$("#destinationInput").val("");
 	$("#firstTrainInput").val("");
 	$("#frequencyInput").val("");
 
-	
+
 	return false;
 });
 
@@ -60,7 +59,7 @@ trainData.on("child_added", function(childSnapshot, prevChildKey){
 	var tRemainder = moment().diff(moment.unix(tFirstTrain), "minutes") % tFrequency ;
 	var tMinutes = tFrequency - tRemainder;
 
-	var tArrival = moment().add(tMinutes, "m").format("hh:mm A"); 
+	var tArrival = moment().add(tMinutes, "m").format("hh:mm A");
 	console.log(tMinutes);
 	console.log(tArrival);
 
@@ -69,5 +68,5 @@ trainData.on("child_added", function(childSnapshot, prevChildKey){
 	console.log(moment().format("X"));
 
 	$("#trainTable > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>");
-}
 });
+
